6f9ce2eb364c737a1cfa2c0ed89eec998b695d43
BryzMcln <jhnbrypntjmcln@gmail.com>
2023-12-23 15:14:11 +0800

    working ok na

--- histogram.py
+++ histogram.py
@@ -1,230 +1,42 @@
 import pandas as pd
 import re
+import nltk
 from nltk.probability import FreqDist
 from matplotlib import pyplot as plt
 import seaborn as sns
+from nltk.corpus import stopwords  # words in english
+
 
 # List of common English stop words
-stop_words = [
-    # english
-    "i",
-    "me",
-    "my",
-    "myself",
-    "we",
-    "our",
-    "ours",
-    "ourselves",
-    "you",
-    "your",
-    "yours",
-    "yourself",
-    "yourselves",
-    "he",
-    "him",
-    "his",
-    "himself",
-    "she",
-    "her" "hers",
-    "herself",
-    "it",
-    "its",
-    "itself",
-    "they",
-    "them",
-    "their",
-    "theirs",
-    "themselves",
-    "what",
-    "which",
-    "who",
-    "whom",
-    "this",
-    "that",
-    "these",
-    "those",
-    "get",
-    "got",
-    "am",
-    "is",
-    "are",
-    "was",
-    "were",
-    "be",
-    "been",
-    "being",
-    "have",
-    "has",
-    "had",
-    "having",
-    "really",
-    "do",
-    "does",
-    "did",
-    "doing",
-    "an",
-    "the",
-    "and",
-    "but",
-    "if",
-    "or",
-    "because",
-    "as",
-    "until",
-    "while",
-    "of",
-    "at",
-    "by",
-    "for",
-    "with",
-    "about",
-    "against",
-    "between",
-    "into",
-    "through",
-    "during",
-    "before",
-    "after",
-    "above",
-    "below",
-    "to",
-    "from",
-    "up",
-    "down",
-    "in",
-    "out",
-    "on",
-    "off",
-    "over",
-    "under",
-    "again",
-    "further",
-    "then",
-    "once",
-    "here",
-    "there",
-    "when",
-    "where",
-    "why",
-    "how",
-    "all",
-    "any",
-    "both",
-    "each",
-    "few",
-    "more",
-    "most",
-    "other",
-    "some",
-    "such",
-    "no",
-    "nor",
-    "not",
-    "only",
-    "own",
-    "same",
-    "so",
-    "than",
-    "too",
-    "very",
-    "can",
-    "will",
-    "just",
-    "don",
-    "should",
-    "may",
-    "now",
-    "receive",
-    "received",
-    "well",
-    "much",
-    "buy",
-    "also",
-    # tagalog
-    "bili",
-    "na",
-    "ng",
-    "nang",
-    "sa",
-    "pa",
-    "ba",
-    "po",
-    "mag",
-    "nag",
-    "pag",
-    "lang",
-    "ang",
-    "ako",
-    "ko",
-    "yung",
-    "ung",
-    "naman",
-    "sya",
-    "siya",
-    "niya",
-    "nya",
-    "jan",
-    "nyan",
-    "din",
-    "rin",
-    "lahat",
-    "kaya",
-    "ito",
-    "pala",
-    "sana",
-    "pero",
-    "hindi",
-    "ulit",
-    "mga",
-    "di",
-    "oo",
-    "siguro",
-    "kasi",
-    "dahil",
-    "pwede",
-    "parang",
-    "dumating",
-    "lng",
-    "para",
-    "talaga",
-    "shopee",
-    "order",  # order
-    "quality",  # quality
-    "product",  # product
-    "items",
-    "item",  # item
-    "appearance",  # appearance
-    "feature",  # feature
-    "seller",  # seller
-    "shop",  # shop
-    "price",  # price
-    "colour",
-    "color",  # color
-    "delivery",  # delivery
-    "shipping",  # shipping
-    "money",  # money
-    "value",  # value
-    "material",  # material
-    "performance",  # performance
-    "rider",  # rider
-    "box",  # box
-    "packaging",
-    "packed",
-    "time",  # time
+stop_words = set(stopwords.words("english"))
+
+# Add Tagalog stop words
+tagalog_stop_words = [
+    "bili","na","ng","nang","sa","pa","ba","po","mag","nag","pag","lang","ang","ako","ko","yung","ung","naman",
+    "sya","siya","niya","nya","jan","nyan","din","rin","lahat","kaya","ito","pala","sana","pero","hindi","ulit",
+    "mga","di","oo","siguro","kasi","dahil","pwede","parang","dumating","lng","para","talaga",
+    "may", "also", "got",
 ]
 
-# combine count same word meaning
+unecessary_words = [
+    "quality", "shopee", "order", "seller", "shop",   "colour", "color",
+    "delivery", "shipping",  "money",  "material",   "rider", "box", "packaging", "packed",
+]
+stop_words.update(tagalog_stop_words, unecessary_words)
+
+# Mapping for variations to combine
 same_value = {
     "thank": "thanks",
     "good": "goods",
     "ok": "okay",
     "maganda": "ganda",
     "working": "works",
+    "items": "item",
 }
 
 
-def proecessing_each_and_every_txt(comment):
-    # Exclude all single letters a, b, c ...
+def processing_each_and_every_text(comment):
+    # Exclude all single letters like a, b, c ...
     words = re.findall(r"\b[^\d\W_]+\b", comment.lower())
     return [
         # count the same value
@@ -237,7 +49,7 @@ def proecessing_each_and_every_txt(comment):
 def histogram(dataset, word_count=15):
     all_words = []
     for index, data in dataset.iterrows():
-        words = proecessing_each_and_every_txt(str(data["comment"]))
+        words = processing_each_and_every_text(str(data["comment"]))
         all_words.extend(words)
 
     word_freq = FreqDist(all_words)
@@ -257,4 +69,4 @@ if __name__ == "__main__":
     dataset = pd.read_csv("data.csv", encoding="utf-8")
 
     # histogram parameter
-    histogram(dataset, word_count=40)  # top 20 words
+    histogram(dataset, word_count=20)  # top 20 words
